ac1553707a15ab72422e772c77dd1d1c70c5f3e6
Arthur Marques <arthursouza@ads.fiponline.edu.br>
2025-11-24 19:28:21 -0300

    adicionando a paginação

--- core/settings.py
+++ core/settings.py
@@ -94,6 +94,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
